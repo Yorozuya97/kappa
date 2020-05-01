243a8d012cf69c0d198fad8eaf73c79929abcc84
Yorozuya97 <ridick.age@gmail.com>
2020-05-01 13:23:50 +0300

    ascii

--- ASCII.c
+++ ASCII.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
    char c;
-   printf("Enter a character:");
+   printf("Enter a character::");
    scanf("%c",&c);
    printf("The ASCII value of %c = %d",c,c);
     return 0;
